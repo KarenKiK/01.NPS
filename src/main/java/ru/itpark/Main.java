c9d8ae2e076da2bf7015ca6cd07cc71157c23f51
student <student@localhost>
2019-03-02 09:26:44 +0300

    cycles

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -3,44 +3,25 @@ package ru.itpark;
 public class Main {
     public static void main(String[] args) {
         // массив - это объект (объект.свойство, объект.метод())
-        int[] scores = {9, 1, 9}; // Java сама вычислит, что это массив из 3 элементов
-
-        int firstScore = scores[0]; // scores[0] - читаем первый элемент
-        int secondScore = scores[1];
-        int thirdScore = scores[2];
+        int[] scores = {9, 1, 9, 9, 9}; // Java сама вычислит, что это массив из 3 элементов
 
         int promoters = 0; // мы ещё не считали
         int neutrals = 0; // мы ещё не считали
         int detractors = 0; // мы ещё не считали
 
-        if (firstScore >= 9 && firstScore <= 10) {
-            promoters++; // увеличение значения на 1
-        }
-        if (firstScore >= 7 && firstScore <= 8) {
-            neutrals++; // увеличение значения на 1
-        }
-        if (firstScore >= 0 && firstScore <= 6) {
-            detractors++; // увеличение значения на 1
-        }
-
-        if (secondScore >= 9 && secondScore <= 10) { // alt + shift + click
-            promoters++; // увеличение значения на 1
-        }
-        if (secondScore >= 7 && secondScore <= 8) {
-            neutrals++; // увеличение значения на 1
-        }
-        if (secondScore >= 0 && secondScore <= 6) {
-            detractors++; // увеличение значения на 1
-        }
-
-        if (thirdScore >= 9 && thirdScore <= 10) { // alt + shift + click
-            promoters++; // увеличение значения на 1
-        }
-        if (thirdScore >= 7 && thirdScore <= 8) {
-            neutrals++; // увеличение значения на 1
-        }
-        if (thirdScore >= 0 && thirdScore <= 6) {
-            detractors++; // увеличение значения на 1
+        // iter + tab
+        // для каждой оценки из scores
+        for (int score : scores) {
+            // score последовательно будет принимать все значения из scores
+            if (score >= 9 && score <= 10) {
+                promoters++; // увеличение значения на 1
+            }
+            if (score >= 7 && score <= 8) {
+                neutrals++; // увеличение значения на 1
+            }
+            if (score >= 0 && score <= 6) {
+                detractors++; // увеличение значения на 1
+            }
         }
 
         int nps = (promoters - detractors) * 100 / scores.length; // .length - кол-во элементов (или длина массива)
